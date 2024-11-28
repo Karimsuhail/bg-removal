5c4434714e2194133f1c07d87c207a18efc92030
Karimsuhail <karimsuhaildev@gmail.com>
2024-11-28 14:49:35 +0530

    Webhooks Added

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -8,7 +8,7 @@ const clerkWebhooks = async (req,res) => {
     try {
         
         // Create a Svix instance with clerk webhook secret.
-        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
         await whook.verify(JSON.stringify(req.body),{
             "svix-id" : req.headers["svix-id"],
