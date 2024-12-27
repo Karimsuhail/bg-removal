e91aa4c66b143884a798ad2eecf59823d1f6dd35
Karimsuhail <karimsuhaildev@gmail.com>
2024-12-28 00:13:48 +0530

    Project Completed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import Header from '../components/Header'
-import Steps from '../components/Steps'
-import BgSlider from '../components/BgSlider'
-import Testimonials from '../components/Testimonials'
-import Upload from '../components/Upload'
+/* eslint-disable no-unused-vars */
+import React from "react";
+import Header from "../components/Header";
+import Steps from "../components/Steps";
+import BgSlider from "../components/BgSlider";
+import Testimonials from "../components/Testimonials";
+import Upload from "../components/Upload";
 
 const Home = () => {
   return (
@@ -14,7 +15,7 @@ const Home = () => {
       <Testimonials />
       <Upload />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js'
 import userRouter from './routes/userRoutes.js'
+import imageRouter from './routes/imageRoutes.js'
 
 
 // App Config
@@ -12,10 +13,11 @@ await connectDB()
 
 // Initialize Middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors());
 
 // API routes
 app.get('/',(req,res) => res.send("API working"))
 app.use('/api/user',userRouter)
+app.use('/api/image',imageRouter)
 
 app.listen(PORT, () => console.log("Server Running on port " +PORT))
